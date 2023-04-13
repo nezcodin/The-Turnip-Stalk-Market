3e04aa6163df730f6cf1125d3184cfd916497e5d
nezcodin <ch.fusco104@gmail.com>
2023-04-13 11:24:17 -0500

    working on home, random character portrait done

--- stalk-market/src/components/Home.js
+++ stalk-market/src/components/Home.js
@@ -1,13 +1,36 @@
+import React from 'react';
+
 export const Home = (props) => {
+  const portraits = require.context("../assets/portraits", false);
+  const portraitKeys = portraits.keys();
+  const randomIndex = Math.floor(Math.random() * portraitKeys.length);
+  const selectedPortrait = portraits(portraitKeys[randomIndex]);
 
   return (
     <div>
       {props.username ? (
-        <p>Hi, {props.username}!</p>
+        <div>
+          <p>Hi, {props.username}!</p>
+          <img src={selectedPortrait} alt='Random portrait' className='h-62 w-52 m-4' />
+        </div>
       ) : (
-        <p>You are not logged in!</p>
+        <div className='p-10'>
+          <div className='bg-sienna text-white rounded-xl w-auto p-4 flex-wrap max-w-screen-md'>
+            <div>
+              <h2 className='text-4xl'>Welcome to The Turnip Stalk Market!</h2>
+              <p className='font-motivasanslight italic'>Click on the buttons to learn more about the site!</p>
+              <img src={selectedPortrait} alt='Random portrait' className='h-62 w-52 m-4' />
+            </div>
+            <div className='flex flex-col items-center'>
+              <button className='m-6 bg-vividorange text-white p-3 rounded-lg font-finkheavy text-xl w-40 hover:bg-orangehover'>About the Site</button>
+              <button className='m-6 bg-vividorange text-white p-3 rounded-lg font-finkheavy text-xl w-56 hover:bg-orangehover'>What can I do here?</button>
+            </div>
+          </div>
+          <div>
+
+          </div>
+        </div>
       )}
     </div>
   );
-
-}
+};
