964039baec80945596513ca694fcfc29af3518ad
nezcodin <ch.fusco104@gmail.com>
2023-04-13 15:54:43 -0500

    adjusted user model to allow createsuperuser

--- stalk_market_project/stalk_market/models.py
+++ stalk_market_project/stalk_market/models.py
@@ -1,26 +1,95 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+# # Create your models here.
+
+# class User(AbstractUser):
+#     username = models.CharField(max_length=255)
+#     email = models.CharField(max_length=255, unique=True)
+#     password = models.CharField(max_length=255)
+
+#     friend_code = models.IntegerField(null=True, blank=True)
+#     island_name = models.TextField(max_length=10)
+#     profile_picture = models.FileField(upload_to='assets/profile_pics/', default='assets/default_profile_pic.jpg', max_length=255)
+#     bio = models.CharField(max_length=500, null=True, default='No bio.')
+
+#     USERNAME_FIELD = 'email'
+
+#     REQUIRED_FIELDS = []
+
+#     groups = models.ManyToManyField(Group, blank=True, related_name='custom_user_groups')
+#     user_permissions = models.ManyToManyField(Permission, blank=True, related_name='custom_user_user_permissions')
+
+    
+# class Post(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+#     island_name = models.CharField(max_length=10)
+#     title = models.CharField(max_length=50)
+#     turnip_price = models.IntegerField()
+#     post_picture = models.FileField(upload_to='post_pics/')
+#     description = models.TextField(null=True, blank=True)
+#     date = models.DateField(auto_now_add=True)
+#     time = models.TimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.title
+
+# class Comment(models.Model):
+#   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+#   post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+#   text_body = models.CharField(max_length=500)
+#   date = models.DateField(auto_now_add=True)
+#   time = models.TimeField(auto_now_add=True)
+#   parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+#   def __str__(self):
+#         return self.text_body
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Group, Permission
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and save a regular User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-
     friend_code = models.IntegerField(null=True, blank=True)
     island_name = models.TextField(max_length=10)
     profile_picture = models.FileField(upload_to='assets/profile_pics/', default='assets/default_profile_pic.jpg', max_length=255)
     bio = models.CharField(max_length=500, null=True, default='No bio.')
 
     USERNAME_FIELD = 'email'
-
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     groups = models.ManyToManyField(Group, blank=True, related_name='custom_user_groups')
     user_permissions = models.ManyToManyField(Permission, blank=True, related_name='custom_user_user_permissions')
 
-    
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     island_name = models.CharField(max_length=10)
@@ -34,13 +103,15 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-  text_body = models.CharField(max_length=500)
-  date = models.DateField(auto_now_add=True)
-  time = models.TimeField(auto_now_add=True)
-  parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
 
-  def __str__(self):
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text_body = models.CharField(max_length=500)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    def __str__(self):
         return self.text_body
+
