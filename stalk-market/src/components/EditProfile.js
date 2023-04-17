9bd45dbacce30be0c87c3d827e68d5e8c8e64ef0
nezcodin <ch.fusco104@gmail.com>
2023-04-17 02:01:12 -0500

    can update user now

--- stalk-market/src/components/EditProfile.js
+++ stalk-market/src/components/EditProfile.js
@@ -13,24 +13,23 @@ export const EditProfile = (props) => {
 
   const navigate = useNavigate()
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    try {
-      const { password, ...data } = profileData
+    // create a new object with all the profile data except for the password
+    const { password, ...data } = profileData;
 
-      await axios.put(`http://localhost:8000/api/users/${props.user_id}/`, {
-        ...data,
-        current_password: password
-      }, {
-        headers: { 'Content-Type': 'application/json' },
-        withCredentials: true
+    // set a placeholder value for the password field
+    const requestData = { ...data, password: '********' };
+
+    axios.put(`http://localhost:8000/api/users/${props.user_id}/`, requestData)
+      .then(response => {
+        console.log(response.data);
+        navigate('/profile/');
+      })
+      .catch(error => {
+        console.error(error);
       });
-
-      navigate('/profile/')
-    } catch (error) {
-      console.error(error)
-    }
   };
 
   const handleChange = (e) => {
@@ -81,16 +80,6 @@ export const EditProfile = (props) => {
             value={profileData.bio}
             onChange={handleChange}
             className='m-2 rounded-lg pl-3 pt-1 text-black h-24 mb-0'
-          />
-        </label>
-        <label className='text-white m-2'>
-          Current Password:
-          <input
-            type="password"
-            name="password"
-            value={profileData.password}
-            onChange={handleChange}
-            className='m-2 rounded-lg pl-3 text-black'
           />
         </label>
         <button type="submit" className='m-6 bg-vividorange text-white p-3 rounded-lg font-finkheavy text-2xl hover:bg-orangehover'>Save Changes</button>
